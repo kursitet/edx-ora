ec4a0dcf865d368edf47103ba0b32e4f3d67580b
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-12-07 19:22:25 +0300

    I don't understand the point of the location filtering here at all.
    One bigger query is better than lots of small queries most of the time.

--- controller/expire_submissions.py
+++ controller/expire_submissions.py
@@ -78,16 +78,23 @@ def reset_skipped_subs():
     """
     Reset submissions marked skipped to return them to the queue.
     """
-    counter=0
-    unique_locations=[x['location'] for x in Submission.objects.all().values('location').distinct()]
-    for location in unique_locations:
-        subs_pending_total= Submission.objects.filter(
-            location=location,
-            state=SubmissionState.skipped
-        ).order_by('-date_created')
-        for sub in subs_pending_total:
-            sub.state=SubmissionState.waiting_to_be_graded
-            counter+=1
+    
+    counter = 0
+    for submission in Submission.objects.filter(state=SubmissionState.skipped):
+        submission.state=SubmissionState.waiting_to_be_graded
+        counter += 1
+    
+    # Mihara: Seriously, why did they write it like that?
+    #counter=0
+    #unique_locations=[x['location'] for x in Submission.objects.all().values('location').distinct()]
+    #for location in unique_locations:
+    #    subs_pending_total= Submission.objects.filter(
+    #        location=location,
+    #        state=SubmissionState.skipped
+    #    ).order_by('-date_created')
+    #    for sub in subs_pending_total:
+    #        sub.state=SubmissionState.waiting_to_be_graded
+    #        counter+=1
     if counter>0:
         statsd.increment("open_ended_assessment.grading_controller.expire_submissions.reset_skipped_subs",
             tags=["counter:{0}".format(counter)])
